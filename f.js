b8a0301d50457b8741a479d8b8350914e9dbc2f9
Diego Fernando Bocanegra Trendo <diego.b@ppsti.com>
2020-08-13 16:04:29 -0500

    Fix data incoming

--- f.js
+++ f.js
@@ -22,14 +22,14 @@ function testV2V(){
 }
 function enviarDatos(auto_remitiente, auto_receptor, datos){
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    generarOnda();
+    generarOnda(datos);
 
     con1.classList.remove("alert-danger");
     con1.classList.add("alert-success");
     e1.textContent = "Enviando "+datos +"...";
 }
 
-function generarOnda(){
+function generarOnda(datos){
     num_ondas = 5;
     radio_inicial = 20;
     onda_actual = 0;
@@ -39,14 +39,9 @@ function generarOnda(){
 
         ctx.beginPath();
 
-        var dir = "right";
-        if(dir === "right"){
-            ctx.arc(100 + (20*onda_actual), 100, radio_inicial, -Math.PI * (90 / 180), Math.PI* (90/180));
-        }
-        else{
-            ctx.arc(100 + (20*onda_actual), 100, radio_inicial, -Math.PI * (90 / 180), Math.PI* (90/180),true);
-        }
-        
+
+        ctx.arc(100 + (20*onda_actual), 100, radio_inicial, -Math.PI * (90 / 180), Math.PI* (90/180));
+ 
         
         
         ctx.lineWidth = 3;
@@ -58,10 +53,25 @@ function generarOnda(){
         if(onda_actual == num_ondas){
             clearInterval(animacion_onda);
             console.log("deteniendo interval");
-            var con1 = document.getElementById("con-1");
-            var e1 = document.getElementById("estado-c1");
-            con1.classList.remove("alert-danger");
-            con1.classList.add("alert-success");
+            con1.classList.add("alert-danger");
+            con1.classList.remove("alert-success");
+            e1.textContent = "Inactivo";
+
+            recibirInformacion(datos);
         }
     },500);
+}
+
+
+function recibirInformacion(datos){
+    con2.classList.remove("alert-danger");
+    con2.classList.add("alert-warning");
+    e2.textContent = "Recibiendo datos...";
+    setTimeout(function(){
+        con2.classList.remove("alert-warning");
+        con2.classList.add("alert-success");
+        e2.innerHTML = "Datos recibidos <b>"+datos+"</b>";
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    },2000);
 }
